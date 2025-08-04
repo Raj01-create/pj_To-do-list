3798f3cda32c1341dbb290eff30b99620bc1c8c9
Raj01-create <rajendrakeshari.pal014183@rec.ac.in>
2025-08-04 20:31:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 
+const inputBox = document.getElementById("input-box");
+const list = document.getElementById("list");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something");
+    }else{
+        let li=document.createElement("li")
+        li.innerHTML = inputBox.value
+        list.appendChild(li)
+        
+        let span=document.createElement("span")
+        span.innerHTML='&#10006'
+        li.appendChild(span)
+
+    }
+    inputBox.value='';
+}
+
+list.addEventListener('click',function(e){
+    if(e.target.tagName ==="SPAN"){
+        e.target.parentElement.remove();
+    }
+},false);
+
+
+
